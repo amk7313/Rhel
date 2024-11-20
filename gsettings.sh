ecc157192cbbef256d241740cb8e2bb354ca4b49
root <root@localhost.localdomain>
2024-11-20 20:46:42 +0530

    Updated dotfiles

---
+++ gsettings.sh
@@ -0,0 +1,8 @@
+# Set default theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+# Set default font
+gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
+
+# Install GNOME extensions
+sudo apt install -y gnome-shell-extensions
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update and install essential packages
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git curl zsh vim
+
+# Clone dotfiles repo
+git clone https://github.com/yourusername/dotfiles.git ~/dotfiles
+
+# Symlink dotfiles
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# Install and configure Zsh
+sudo apt install -y zsh
+chsh -s $(which zsh)
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Apply other settings
+source ~/.bashrc
