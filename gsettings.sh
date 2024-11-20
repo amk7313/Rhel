def10e5920d02b08da70cad768e4c456560a7806
root <root@localhost.localdomain>
2024-11-20 21:03:55 +0530

    Removed gsettings

--- gsettings.sh
+++
@@ -1,8 +0,0 @@
-# Set default theme
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-
-# Set default font
-gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
-
-# Install GNOME extensions
-sudo apt install -y gnome-shell-extensions
